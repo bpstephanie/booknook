fbde2d773bc6b92ada904fe5877055faae827fe3
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-02-26 23:17:03 +0000

    Added product detail file, modified products file to display books and accessories only, amended views, models and urls files to correctly display products in product detail file

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('books/', views.all_products, name='books'),
-    path('accessories/', views.all_products, name='accessories'),
+    path(
+        'books/', 
+        views.all_products, 
+        name='all_books', 
+        kwargs={'type': 'books'}
+    ),
+    path(
+        'accessories/',
+        views.all_products,
+        name='all_accessories',
+        kwargs={'type': 'accessories'}
+    ),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
