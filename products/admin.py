62870e585130592b37da10423842cb87aa3222aa
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-02-26 02:05:01 +0000

    Customized products admin

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,56 @@
 from django.contrib import admin
 from .models import Genre, Category, Book, Accessory
 
+
 # Register your models here.
-admin.site.register(Genre)
-admin.site.register(Category)
-admin.site.register(Book)
-admin.site.register(Accessory)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'parent_genre',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'parent_category',
+    )
+
+    ordering = ('name', 'parent_category')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+        'genre',
+        'isbn',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name', 'author', 'genre', )
+
+
+class AccessoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'sku',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name', 'category', 'sku',)
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Accessory, AccessoryAdmin)
