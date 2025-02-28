6fcb7f8c1669e5c4712b65963d8c76b9b6f8db7b
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-02-28 01:00:46 +0000

    Altered genres field in products models file, added noimage file

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product, Book, Accessory
+from .models import Product, Book, Accessory, Category, Genre
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
@@ -10,17 +10,10 @@ def all_products(request, type=None):
     """ A view to show all products, including sorting and search queries """
     product_type = type if type else request.GET.get('type', 'all')
     query = request.GET.get('q') if 'q' in request.GET else None
+    categories = None
+    genres = None
 
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET.get('q')
-            if not query:
-                messages.error(request, "You didn't search for anything")
-                return redirect(reverse('products'))
-
-    # Initialize an empty queryset
-    products = Product.objects.none()
-
+    # Initialize the products queryset based on type
     if product_type == 'books':
         products = Book.objects.all()
         page_title = 'All Books'
@@ -31,8 +24,30 @@ def all_products(request, type=None):
         products = Product.objects.all()
         page_title = 'All Products'
 
+    if request.GET:
+        if 'category' in request.GET:  # and product_type == 'accessories':
+            category_names = request.GET.get('category').split(',')
+            if product_type == 'accessories':
+                products = products.filter(category__name__in=category_names)
+                categories = Category.objects.filter(name__in=category_names)
+        if 'genre' in request.GET:  # and product_type == 'books':
+            genre_names = request.GET.get('genre').split(',')
+            products = products.filter(genre__name__in=genre_names)
+            genres = Genre.objects.filter(name__in=genre_names)
+
+        if 'q' in request.GET:
+            query = request.GET.get('q')
+            if not query:
+                messages.error(request, "You didn't search for anything")
+                return redirect(reverse('products'))
+
     if query:
-        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        products = products.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+        if not products.exists():
+            messages.error(request, 'No products match your search criteria')
+            return redirect(reverse('products'))
 
     paginator = Paginator(products, 16)
     page_number = request.GET.get('page')
@@ -42,6 +57,8 @@ def all_products(request, type=None):
         'products': page_obj,
         'page_title': page_title,
         'search_term': query,
+        'current_categories': categories,
+        'current_genres': genres,
     }
 
     return render(request, 'products/products.html', context)
