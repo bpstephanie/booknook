7af411b8fce700abaa4ccb8aabd56ecc72c29c9c
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-03 02:17:27 +0000

    Added Review, Comment, Edit and delete functionality, forms and html

--- products/urls.py
+++ products/urls.py
@@ -16,10 +16,30 @@ urlpatterns = [
         kwargs={'type': 'accessories'}
     ),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('books/all-genres', views.all_genres, name='all_genres'),
+    path('books/all-genres/', views.all_genres, name='all_genres'),
     path(
-        'accessories/all-categories',
+        'accessories/all-categories/',
         views.all_categories,
         name='all_categories'
+    ),
+    path(
+        'review/<int:review_id>/edit/',
+        views.edit_review,
+        name='edit_review'
+    ),
+    path(
+        'review/<int:review_id>/delete/',
+        views.delete_review,
+        name='delete_review'
+    ),
+    path(
+        'comment/<int:comment_id>/edit/',
+        views.edit_comment,
+        name='edit_comment'
+    ),
+    path(
+        'comment/<int:comment_id>/delete/',
+        views.delete_comment,
+        name='delete_comment'
     ),
 ]
