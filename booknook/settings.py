ee1ffdf413420b31875c59667f4522915ca94a7d
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-11 22:37:51 +0000

    Added checkout view and template

--- booknook/settings.py
+++ booknook/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'wishlist',
     'bag',
     'checkout',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -62,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'booknook.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth  # noqa
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
