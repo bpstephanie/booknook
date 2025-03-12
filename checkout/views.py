82f12cc7b280dc58eaa787fafb595d83cec499fa
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-12 20:31:54 +0000

    Added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse
+)
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -8,9 +11,26 @@ from products.models import Product
 from bag.contexts import bag_contents
 
 import stripe
+import json
 
 
-# Create your views here.
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
+
 def checkout(request):
     """ View to handle the checkout process for the user """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -31,7 +51,7 @@ def checkout(request):
             'postcode': request.POST['postcode'],
         }
         order_form = OrderForm(form_data)
-        if order_form.is_valid:
+        if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
@@ -58,7 +78,8 @@ def checkout(request):
 
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(
-                reverse('checkout_success', args=[order.order_number]))
+                reverse('checkout_success', args=[order.order_number])
+            )
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
@@ -80,21 +101,21 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-    order_form = OrderForm()
+        order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
-    template = 'checkout/checkout.html'
+        template = 'checkout/checkout.html'
 
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
-    return render(request, template, context)
+        return render(request, template, context)
 
 
 def checkout_success(request, order_number):
