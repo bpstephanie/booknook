f3c547285d8654c54ae806f586b42c292a4efe3f
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-15 05:27:34 +0000

    Added and removed thumbnail from pdf models

--- profiles/models.py
+++ profiles/models.py
@@ -52,6 +52,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class PDF(models.Model):
     title = models.CharField(max_length=255)
@@ -60,3 +63,4 @@ class PDF(models.Model):
 
     def __str__(self):
         return self.title
+
