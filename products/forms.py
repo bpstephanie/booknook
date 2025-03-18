96b8d07a55add304c94a9e33d11e5a3cf5cef94e
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-18 16:21:47 +0000

    AddedBookForm to products app

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,21 @@
 from django import forms
-from .models import Review, ReviewComment
+from .models import Book, Accessory, Genre, Category, Review, ReviewComment
+
+
+class BookForm(forms.ModelForm):
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        genres = Genre.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for g in genres]
+
+        self.fields['genre'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-navy rounded-0'
 
 
 class ReviewForm(forms.ModelForm):
