1f1f15ae1db6fb91aaff7bfca3fc45d56f1be4cc
bpstephanie <stephaniebellpalomino@hotmail.com>
2025-03-19 19:58:41 +0000

    Added newsletter signup model, url, view, content to template and implemented functionality to send emails. Edited styling infor app templates

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import NewsletterSignup
+
 
 # Register your models here.
+class NewsletterSignupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'date_signed_up')
+    search_fields = ('name', 'email')
+    list_filter = ('date_signed_up')
+
+
+admin.site.register(NewsletterSignup)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import NewsletterSignupForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
 
 # Create your views here.
+def send_newsletter_signup_email(name, email):
+    subject = render_to_string('home/emails/newsletter_signup_subject.txt')
+    body = render_to_string('home/emails/newsletter_signup_body.txt', {'name': name})
+    plain_body = strip_tags(body)
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [email]
+
+    send_mail(subject, plain_body, from_email, recipient_list)
+
+
 def index(request):
     """ A view that displays the index page """
-    return render(request, 'home/index.html')
+    form = NewsletterSignupForm()
+    return render(request, 'home/index.html', {'form': form})
+
+
+def newsletter_signup(request):
+    """ A view that displays the newsletter signup """
+    if request.method == 'POST':
+        form = NewsletterSignupForm(request.POST)
+        if form.is_valid():
+            signup = form.save()
+            send_newsletter_signup_email(signup.name, signup.email)
+            messages.success(
+                request,
+                'Thank you for signing up to the BookNook Newsletter!')
+        else:
+            messages.error(request, 'Oops! Looks that email has already been signed up.')
+    else:
+        form = NewsletterSignupForm()
+    return render(request, 'home/index.html', {'form': form})
